07e760cd7d7c2ca30b8aa05fb8f5d17fcf24ee45
anthony <aamining2@gmail.com>
2019-07-19 20:32:19 +0200

    supreme http  client improvements

--- src/Http/SupremeNewYorkHttpClient.php
+++ src/Http/SupremeNewYorkHttpClient.php
@@ -5,18 +5,24 @@ namespace Supreme\Parser\Http;
 
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use Supreme\Parser\Parsers\LatestDroplistUrlParser;
+use Supreme\Parser\ProductParser;
 
 class SupremeNewYorkHttpClient
 {
     public $client;
+
+    public $debug = false;
 
     protected $baseUri = "https://www.supremenewyork.com";
 
     public function __construct()
     {
         $this->client = new Client([
-            'base_uri' => $this->baseUri,
             'headers' => [
                 'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
                 'accept-encoding' => 'gzip, deflate, br',
@@ -30,8 +36,68 @@ class SupremeNewYorkHttpClient
             ]]);
     }
 
+    public function setBaseUri(string $uri)
+    {
+        $this->baseUri = $uri;
+    }
+
+    public function setDebugMode(bool $debug)
+    {
+        $this->debug = $debug;
+    }
+
+
     public function getNews(int $page)
     {
-        return $this->client->get("/news/page/" . $page);
+        return $this->client->get($this->baseUri . "/news/page/" . $page);
+    }
+
+    public function getStock()
+    {
+        return $this->client->get($this->baseUri . "/shop.json");
+    }
+
+    public function getMobileStock()
+    {
+        return $this->client->get($this->baseUri . "/mobile_stock.json");
+    }
+
+    public function getItem($id)
+    {
+        return $this->client->get($this->baseUri . "/shop/$id.json");
+    }
+
+    public function getItems(array $ids, $concurrency = 4)
+    {
+        $items = [];
+        $requests = [];
+
+        foreach ($ids as $id) {
+            $requests[] = new Request("GET", $this->baseUri . "/shop/$id.json");
+        }
+
+            $pool = new Pool($this->client, $requests, [
+                'concurrency' => $concurrency,
+                'fulfilled' => function (Response $response, $index) use (&$items) {
+                    $items[] = $response;
+                },
+                'rejected' => function (RequestException $reason, $index) {
+                    if ($this->debug) {
+                        if ($reason->getCode() == 404)
+                            echo "failed to request: $index  -  404 . \n";
+                        else
+                            echo "failed to request: $index  -  $reason . \n";
+                    }
+
+                },
+            ]);
+
+            // Initiate the transfers and create a promise
+            $promise = $pool->promise();
+
+            // Force the pool of requests to complete.
+            $promise->wait();
+
+        return $items;
     }
 }
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -4,39 +4,45 @@ namespace Supreme\Parser\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Supreme\Parser\Http\SupremeCommunityHttpClient;
+use Supreme\Parser\Http\SupremeNewYorkHttpClient;
 use Supreme\Parser\SupremeCommunity;
 use Supreme\Parser\SupremeLookbookParser;
 use Supreme\Parser\SupremeNewYork;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 
 class ParserTest extends TestCase
 {
-    public function testDroplistUrlParser(){
+    public function testDroplistUrlParser()
+    {
         $http = new SupremeCommunityHttpClient();
-       $url = $http->getLatestDroplistUrl();
-       $this->assertStringContainsString("season",$url);
+        $url = $http->getLatestDroplistUrl();
+        $this->assertStringContainsString("season", $url);
     }
 
-    public function testGetLatestDroplistIds(){
+    public function testGetLatestDroplistIds()
+    {
         $client = new SupremeCommunity(2, true);
         $ids = $client->getItemIds();
 
         $this->assertNotEmpty($ids);
 
-        foreach($ids as $id ){
+        foreach ($ids as $id) {
             $this->assertIsNumeric($id);
         }
     }
 
-    public function testGetLatestDroplistItems(){
+    public function testGetLatestDroplistItems()
+    {
         $client = new SupremeCommunity(2, true);
         $items = $client->getDropListItems();
         $this->assertNotEmpty($items);
     }
 
-    public function testGetItemsBySeasonDate(){
+    public function testGetItemsBySeasonDate()
+    {
         $client = new SupremeCommunity(2, true);
-        $items = $client->getDropListItems('spring-summer2019','2019-07-05');
+        $items = $client->getDropListItems('spring-summer2019', '2019-07-05');
         $this->assertNotEmpty($items);
     }
 
@@ -50,10 +56,38 @@ class ParserTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function testParseNews(){
+    public function testParseNews()
+    {
         $supreme = new SupremeNewYork();
         $news = $supreme->parseNews();
 
         $this->assertNotEmpty($news);
+    }
+
+    public function getItems()
+    {
+        $client = new SupremeNewYorkHttpClient();
+        $client->setBaseUri('http://api.supremewatcher.test/v1/supreme');
+        $client->setDebugMode(true);
+        $stock = $client->getStock()->getBody()->getContents();
+
+        $ids = [];
+
+        foreach (json_decode($stock, true)['products_and_categories'] as $category => $items) {
+            foreach ($items as $item) {
+                $ids[] = $item['id'];
+            }
+
+        }
+        $ids = array_unique($ids);
+
+        $stopwatch = new Stopwatch(true);
+        $stopwatch->start('parse');
+        $items = $client->getItems($ids, 12);
+        $stopwatch->stop('parse');
+
+        echo $stopwatch->getEvent('parse')->getDuration();
+
+        $this->assertTrue(true);
     }
 }
